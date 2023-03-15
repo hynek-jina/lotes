b753437f4c32acdb3884c06e4d0daae08a561111
Hynek Jina <hynekjina@Hyneks-MacBook-Air.local>
2023-03-15 11:12:28 +0100

    package update

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up KIKX App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Playground" component={PlaygroundScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/LoginScreen.ts
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function Login(): JSX.Element {
+  return (
+    <View>
+      <Text>Login page</Text>
+    </View>
+  );
+}
+
+export default Login;
