a85d204d6abd2d1cd05ed76055f83145027cfc95
Hynek Jina <hynekjina@Hyneks-MacBook-Air.local>
2023-03-16 12:00:01 +0100

    hello jotai

--- App.tsx
+++ App.tsx
@@ -3,18 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./src/screens/LoginScreen";
+import {atom, useAtom} from 'jotai'
+
+
+// state/formData
+type State = {
+  username: string
+  password: string
+}
+
+export const formDataAtom = atom<State>({username: '', password: ''})
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+const hasValue = false
+
+const [formData, setFormData]= useAtom(formDataAtom)
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
+        {!hasValue ? <Stack.Screen
           name="Login"
           component={Login}
           options={{ headerShown: false }}
-        />
+        /> : <Stack.Screen name="Settings" component={Login} />} {/* sem pak jebni homescreen */}
         {/* <Stack.Screen name="Settings" component={SettingsScreen} />
         <Stack.Screen name="Playground" component={PlaygroundScreen} /> */}
       </Stack.Navigator>
