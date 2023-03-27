0701475262c48fb6b1e7a087d41665bacdae8caa
Hynek Jína <hynjin@gmail.com>
2023-03-27 06:27:31 +0100

    nfcforumtype4 initial

--- src/nfc.ts
+++ src/nfc.ts
@@ -1,19 +1,20 @@
-import NfcManager, { NfcTech, Ndef } from "react-native-nfc-manager";
+import NfcManager, { NfcTech, NfcV } from "react-native-nfc-manager";
 import { Platform } from "react-native";
 
 export const readNfc = async (): Promise<string> => {
   let text = "";
   let resp: number[] | string | null = null;
   try {
-    let tech = Platform.OS === "ios" ? NfcTech.MifareIOS : NfcTech.NfcA;
+    let tech = Platform.OS === "ios" ? NfcTech.MifareIOS : NfcTech.NfcV;
     resp = await NfcManager.requestTechnology(tech, {
       alertMessage: "Scan the Lote",
     });
 
-    let cmd =
-      Platform.OS === "ios"
-        ? NfcManager.sendMifareCommandIOS
-        : NfcManager.transceive;
+    // let cmd =
+    //   Platform.OS === "ios"
+    //     ? NfcManager.sendMifareCommandIOS
+    //     : NfcManager.transceive;
+    let cmd = NfcManager.transceive;
 
     resp = await cmd([0x3a, 4, 4]);
     let payloadLength = parseInt(resp.toString().split(",")[1]);
@@ -23,7 +24,7 @@ export const readNfc = async (): Promise<string> => {
 
     resp = await cmd([0x3a, startPage, endPage]);
     let bytes = resp.toString().split(",");
-    
+
     for (let i = 0; i < bytes.length; i++) {
       if (i < 5) {
         continue;
@@ -43,17 +44,21 @@ export const readNfc = async (): Promise<string> => {
   if (resp) {
     NfcManager.cancelTechnologyRequest();
   }
-  
+
   return text;
 };
 
-export const writeNdef = async (message: string, alert:string): Promise<boolean> => {
+export const writeNdef = async (
+  message: string,
+  alert: string
+): Promise<boolean> => {
   try {
-    const tech = await NfcManager.requestTechnology(NfcTech.Ndef, {
+    const tech = await NfcManager.requestTechnology(NfcTech.NfcV, {
       alertMessage: alert,
     });
 
-    const bytes = Ndef.encodeMessage([Ndef.textRecord(message)]);
+    const bytes = NfcV.encodeMessage([NfcV.textRecord(message)]);
+
     await NfcManager.ndefHandler.writeNdefMessage(bytes);
 
     await NfcManager.setAlertMessageIOS("Successfully stored.");
