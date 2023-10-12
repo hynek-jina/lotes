cfde5f3ff56f010b10d5a1bc38ddf1c52061b1ee
Hynek Jína <hynjin@gmail.com>
2023-10-12 13:40:51 +0100

    Update src/atoms.ts
    Co-authored-by: David Novák <mail@davenov.com>

--- src/atoms.ts
+++ src/atoms.ts
@@ -3,7 +3,19 @@ import { atomWithMMKV } from "./atomWithMMKV";
 export const apiKeyAtom = atomWithMMKV("apiKey", "");
 export const serverAtom = atomWithMMKV("server", "");
 
-export const lnbitsUrlAtom = atomWithMMKV("lnbitsUrl", "");
+
+interface UserInfo {
+  domain: string
+  user: string
+  wallet: string
+}
+
+export const lnbitsUrlAtom = atomWithMMKV<string | null>("lnbitsurl", null)
+
+export const userInfoAtom = selectAtom(lnbitsUrlAtom, (url) => {
+  if(!url) return null
+  return parseLnbitsUrl(url)
+})
 export const domainAtom = atomWithMMKV("domain", "");
 export const userAtom = atomWithMMKV("user", "");
 export const walletAtom = atomWithMMKV("wallet", "");
