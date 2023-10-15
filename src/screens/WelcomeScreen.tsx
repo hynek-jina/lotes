fdbc41e75368cbf2aa600bb37bc750f4c0f1b3d8
Hynek Jina <hynekjina@Hyneks-MacBook-Air.local>
2023-10-15 07:37:34 +0100

    wip

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions, Image } from "react-native";
 import { styles } from "../styles";
 
 import { createUser } from "../api";
 import constructLnbitsUrl from "../utils/constructLnbitsUrl";
+import Carousel from "react-native-snap-carousel";
 
 function Welcome({ navigation }: { navigation: any }) {
   const handleNewUserButton = async () => {
@@ -21,10 +22,58 @@ function Welcome({ navigation }: { navigation: any }) {
     }
   };
 
+  interface CarouselData {
+    id: number;
+    name: string;
+    url: string;
+  }
+
+  const SLIDER_WIDTH = Dimensions.get('window').width + 30;
+  const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
+  
+  const data: CarouselData[] = [
+    {
+      id: 1,
+      name: 'React JS',
+      url: 'https://icon-library.com/images/react-icon/react-icon-29.jpg',
+    },
+    {
+      id: 2,
+      name: 'JavaScript',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png',
+    },
+    {
+      id: 3,
+      name: 'Node JS',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/6/67/NodeJS.png',
+    },
+  ];
+
+const renderItem = ({ item }: { item: CarouselData }) => {
+  return (
+    <View
+      style={{
+        borderWidth: 1,
+        padding: 20,
+        borderRadius: 20,
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Image source={{uri: item.url}} style={{width: 200, height: 200}} />
+      <Text style={{marginVertical: 10, fontSize: 20, fontWeight: 'bold'}}>
+        {item.name}
+      </Text>
+    </View>
+  );
+};
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Welcome</Text>
       <Text style={styles.subHeader}>What is Lotes... actually..</Text>
+      <Carousel data={data} renderItem={renderItem}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}></Carousel>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}
